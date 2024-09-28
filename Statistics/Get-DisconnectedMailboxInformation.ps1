7ba346b887b1bcc2e9dc1c0ca67b36b482ffce1b
Yovel14 <Yovelavitan14@gmail.com>
2024-09-28 16:26:44 +0300

    Adding Script that Gets Information about disconnected mailboxes.

---
+++ Statistics/Get-DisconnectedMailboxInformation.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.SYNOPSIS
+	Gets Information about disconnected mailboxes.
+.DESCRIPTION
+	Gets Information about disconnected mailboxes. (Amount of Disconnected mailboxes, Average Size, Average Mailbox Count per DB)
+.EXAMPLE
+	PS> .\Get-DisconnectedMailboxInformation.ps1
+.LINK
+	https://github.com/Yovel14/ExchangeScripts
+.NOTES
+	Author: Yovel
+#>
+Add-PSSnapin *xch*
+
+$dbs = Get-MailboxDatabase | select -ExpandProperty Name | sort
+$totalAmount = 0
+$mailboxSizeAverage = 0
+foreach($db in $dbs)
+{
+    Write-Host "Starting on DB: $db" -ForegroundColor Cyan
+    $dbd = Get-MailboxStatistics -Database $db -Filter {DisconnectDate -ne $null}
+    $mailboxSizeAverage += ($dbd.TotalItemSize.value | measure -Average).Average
+    $totalAmount +=$dbd.Count
+    Write-Host "Done on DB: $db" -ForegroundColor Cyan
+}
+
+Write-Host "Amount of Disconnected mailboxes: $totalAmount"
+Write-Host "average mailbox size: $(($mailboxSizeAverage/$dbs.Count)/1GB)GB"
+Write-Host "Average users per database: $($totalAmount/$($dbs.Count))"
